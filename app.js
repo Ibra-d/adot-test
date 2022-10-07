74b30c45c14b56915d68c7ea341c7f8b146c3d53
DRAME-ibrahima <drame.ibou.07@gmail.com>
2022-10-07 13:22:02 +0200

    adot

--- app.js
+++ app.js
@@ -2,20 +2,12 @@
 
 const video = document.querySelector(".videoContainer > video");
 const videoContainer = document.querySelector(".videoContainer");
-const navigation = document.querySelector(".adressAndMaps");
+
 const chevron = document.querySelector(".chevron");
 const link = document.querySelector(".linkContainer > a");
 const linkAnimated = document.querySelector(".linkAnimated");
 
 video.onended = function () {
-  videoContainer.style.scale = "0";
-  if ((videoContainer.style.scale = "0")) {
-    setTimeout(() => {
-    videoContainer.style.display='none'
-  navigation.classList.add("navReverse");
-    }, 1000);
-  }
-  chevron.style.display = "none";
- link.textContent = "j'y vais";
- linkAnimated.style.height = "60px";
+  const navigation = document.createElement("div");
+  navigation.classList.add(".adressAndMaps");
 };
