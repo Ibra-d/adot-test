4aa5bd8a95ec20789b19c77bd0afd8f61718fd63
DRAME-ibrahima <drame.ibou.07@gmail.com>
2022-10-08 11:45:51 +0200

    6th Commit

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 // Animation when the video stop
 
 const video = document.querySelector(".videoContainer > video");
-const linkContainer = document.querySelector('.linkContainer')
-const chevron = document.querySelector('.chevron')
+const linkContainer = document.querySelector(".linkContainer");
+const chevron = document.querySelector(".chevron");
 const firstcontainerAnimatedChild = document.querySelector(
   ".firstcontainerAnimatedChild"
 );
 
 video.onended = function () {
-    firstcontainerAnimatedChild.classList.add("containerToUp");
-    linkContainer.classList.add("linkTextChange");
-    chevron.classList.add('chevronUp')
+  firstcontainerAnimatedChild.classList.add("containerToUp");
+  linkContainer.classList.add("linkTextChange");
+  chevron.classList.add("chevronUp");
 };
+
+function myFunction(x) {
+  if (x.matches) {
+  } else {
+  }
+}
+
+var x = window.matchMedia("(max-width: 500px)")
+myFunction(x) 
+x.addListener(myFunction)
